0bfe63cb8a8666518740091d4971b3532ffff8b3
Martin Junker <kontakt@martinjunker.de>
2019-04-06 15:48:12 +0200

    added API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var api = require('./utils/api');
 
 class App extends Component {
+  componentDidMount() {
+    api.fetchAllBoards().then(
+      function(allBoards) {
+        this.setState(function() {
+          return {
+            allBoards: allBoards
+          };
+        });
+      }.bind(this)
+    );
+  }
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
   render() {
     return (
       <div className="App">
