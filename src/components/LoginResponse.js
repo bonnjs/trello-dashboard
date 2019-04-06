4fe472a340e6a3cf6c3755c287a5832fbab827cd
Fawad Mirzad <mirzad@arndtteunissen.de>
2019-04-06 16:48:45 +0200

    add comment to describe variables

--- src/components/LoginResponse.js
+++ src/components/LoginResponse.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 export default function(props) {
     let queryParams = props.location.search.substr(1,);
     let [authToken, verifyToken] = queryParams.split('&').map(token=> token.split('=')[1]);
     const isLoggedIn = authToken && verifyToken;
+
+    //use authToken and verifyToken to request network
 
     const successMessage =  <p> Succesfully logged in</p>;
     const errorMessage = <p> Oh , something went wrong. Did you denied permision? <Link to="/">Back to login page</Link> </p>;
