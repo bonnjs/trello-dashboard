87f9d003833e16fc5d12473ddb683b7cf3db562f
Fawad Mirzad <mirzad@arndtteunissen.de>
2019-04-06 15:37:38 +0200

    refactor login response component

--- src/components/LoginResponse.js
+++ src/components/LoginResponse.js
@@ -4,12 +4,13 @@ export default function(props) {
     let queryParams = props.location.search.substr(1,);
     let [authToken, verifyToken] = queryParams.split('&').map(token=> token.split('=')[1]);
     const isLoggedIn = authToken && verifyToken;
+
+    const successMessage =  <p> Succesfully logged in</p>;
+    const errorMessage = <p> Oh , something went wrong. Did you denied permision? <Link to="/">Back to login page</Link> </p>;
+
     return (
         <div>
-            {isLoggedIn ?
-                <p> Succesfully logged in</p>
-                : <p> Oh , something went wrong. Did you denied permision? <Link to="/">Back to login page</Link> </p>
-            }
+            {isLoggedIn ? successMessage :errorMessage}
         </div>
     );
 }
