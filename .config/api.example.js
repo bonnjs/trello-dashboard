635fb83c8d829e6959b6ed6775ec1f237c5f2882
Fahmi Farjallah <fahmi.farjallah@exagon.de>
2019-04-06 12:57:31 +0200

    add trello oauth secret

--- .config/api.example.js
+++ .config/api.example.js
@@ -1,2 +1,3 @@
 // create a api.js in this folder, copy content of this file and add the correct values.
 export const trelloApiKey = 'api key comes here';
+export const trelloOauthSecret = 'secret comes here';
